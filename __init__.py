356eeff6f972b884093bbad8169dbb8272e9813a
juvi21 <iphone8l@skiff.com>
2024-06-29 11:40:12 +0200

    chore: clean up

--- __init__.py
+++ __init__.py
@@ -1,8 +1,7 @@
 from .core import NNGDB
 from .cli import NNGDBREPL
 from . import utils
-from . import advanced
 
 __version__ = "0.1.0"
 
-__all__ = ['NNGDB', 'NNGDBREPL', 'utils', 'advanced']
+__all__ = ['NNGDB', 'NNGDBREPL', 'utils']
